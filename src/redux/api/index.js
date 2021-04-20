c7ee472090a10d9c7a6450990b039a3f2ba4e83b
weberlance <andrew.petin@effective-soft.com>
2021-04-20 11:47:50 +0200

    fix error handling

--- src/redux/api/index.js
+++ src/redux/api/index.js
@@ -1,19 +1,23 @@
 import axios from 'axios';
 
-// TODO: return rejected promise or throw an error
-// for correct behavior in sagas
-const handleError = error => (
-  error.response
-    ? {
-      error: {
-        error: error.response.status,
-        message: error.response.data,
-      },
-    }
-    : { error }
-  );
+const handleError = error => {
+  if (error.toJSON) {
+    error.toJSON();
+  }
 
-const handleSuccess = response => ({
+  return Promise.reject(
+    error.response
+      ? {
+        error: {
+          status: error.response.status,
+          message: error.response.data,
+        },
+      }
+      : { error }
+  );
+};
+
+const handleSuccess = response => Promise.resolve({
   data: response.data,
   headers: response.headers,
 });
@@ -21,16 +25,14 @@ const handleSuccess = response => ({
 // TODO:
 // 1. create seaparate api files for user and etc
 // 2. possible use axios.create with baseUrl and move base url to .env file
-// 3. check doc and implementation
+
 /**
  * Make HTTP POST call
  * @param {string} url
  * @param {string} query
  * @param {string} token
  *
- * @returns {{json: Object|undefined, error: Object|undefined}} response
- * @returns {Object|undefined} response.json
- * @returns {{status: Object, message: Object}|Object|undefined} response.error
+ * @returns {Promise} Promise object with response data
  *
  * @example
  * get("http://management-api", "?search=bonus", "asd32eD5F");
